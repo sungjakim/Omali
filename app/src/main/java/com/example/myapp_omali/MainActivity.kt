376b41ab09a282323eb9c69aa42038f363202ba1
Omali <misskimlilac68@gmail.com>
2023-04-21 15:23:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapp_omali/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapp_omali
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
